0d8a9fd6444260785e0b42f83e7488ad3c14877b
dbaykih01 <124503678+dbaykih01@users.noreply.github.com>
2025-06-18 04:51:49 +0300

    menu-animations.js

--- menu-animations.js
+++ menu-animations.js
@@ -3,20 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
     manakish: {
       title: '١. منقوش',
       items: [
-        { name: 'زعتر', sizes: { small: '30,000 ل.ل', large: '50,000 ل.ل' }, image: 'logozouhbi.PNG' },
-        { name: 'جبنه', sizes: { small: '70,000 ل.ل', large: '140,000 ل.ل' }, image: 'logozouhbi.PNG' },
-        { name: 'جبنه إكسترا', sizes: { standard: '170,000 ل.ل' }, image: 'logozouhbi.PNG' },
-        { name: 'جبنه دره', sizes: { small: '100,000 ل.ل', large: '170,000 ل.ل' }, image: 'logozouhbi.PNG' },
-        { name: 'جبنه خضرا', sizes: { small: '100,000 ل.ل', large: '170,000 ل.ل' }, image: 'logozouhbi.PNG' },
-        { name: 'جبنه سجق', sizes: { small: '100,000 ل.ل', large: '190,000 ل.ل'}, image: 'logozouhbi.PNG' },
+        { name: 'زعتر', sizes: { small: '30,000 ل.ل', large: '50,000 ل.ل' }, image: 'logozouhbi.png' },
+        { name: 'جبنه', sizes: { small: '70,000 ل.ل', large: '140,000 ل.ل' }, image: 'logozouhbi.png' },
+        { name: 'جبنه إكسترا', sizes: { standard: '170,000 ل.ل' }, image: 'logozouhbi.png' },
+        { name: 'جبنه دره', sizes: { small: '100,000 ل.ل', large: '170,000 ل.ل' }, image: 'logozouhbi.png' },
+        { name: 'جبنه خضرا', sizes: { small: '100,000 ل.ل', large: '170,000 ل.ل' }, image: 'logozouhbi.png' },
+        { name: 'جبنه سجق', sizes: { small: '100,000 ل.ل', large: '190,000 ل.ل'}, image: 'logozouhbi.png' },
         
-        { name: 'لحمه', sizes: { small: '70,000 ل.ل', large: '140,000 ل.ل' }, image: 'logozouhbi.PNG' },
-        { name: 'لحمه مع جبنه', sizes: { small: '100,000 ل.ل', large: '170,000 ل.ل' }, image: 'logozouhbi.PNG' },
-        { name: 'نص نص', sizes: { small: 'ل.ل', large: '100,000 ل.ل'}, image: 'logozouhbi.PNG' },
-        { name: 'قريشه', sizes: { small: '50,000 ل.ل', large: '100,000 ل.ل'}, image: 'logozouhbi.PNG' },
-        { name: 'قريشه مع جبنه', sizes: { small: '70,000 ل.ل', large: '150,000 ل.ل' }, image: 'logozouhbi.PNG' },
-        { name: 'قريشه مع جبنه ودره و زيتون', sizes: { standard: '170,000 ل.ل' }, image: 'logozouhbi.PNG' },
-        { name: 'تركيه', sizes: { small: '70,000 ل.ل', large: '130,000 ل.ل'  }, image: 'logozouhbi.PNG' },
+        { name: 'لحمه', sizes: { small: '70,000 ل.ل', large: '140,000 ل.ل' }, image: 'logozouhbi.png' },
+        { name: 'لحمه مع جبنه', sizes: { small: '100,000 ل.ل', large: '170,000 ل.ل' }, image: 'logozouhbi.png' },
+        { name: 'نص نص', sizes: { small: 'ل.ل', large: '100,000 ل.ل'}, image: 'logozouhbi.png' },
+        { name: 'قريشه', sizes: { small: '50,000 ل.ل', large: '100,000 ل.ل'}, image: 'logozouhbi.png' },
+        { name: 'قريشه مع جبنه', sizes: { small: '70,000 ل.ل', large: '150,000 ل.ل' }, image: 'logozouhbi.png' },
+        { name: 'قريشه مع جبنه ودره و زيتون', sizes: { standard: '170,000 ل.ل' }, image: 'logozouhbi.png' },
+        { name: 'تركيه', sizes: { small: '70,000 ل.ل', large: '130,000 ل.ل'  }, image: 'logozouhbi.png' },
 
 
 
@@ -28,54 +28,54 @@ document.addEventListener('DOMContentLoaded', function () {
     soiree: {
       title: '٢. سواريه',
       items: [
-        { name: 'كروسون جبنه', price: '50,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'كروسون شوكولا', price: '50,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: ' بيتزا', price: '40,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'لحمه بعجين صغير', price: '40,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'لحمه بعجين كبير', price: '80,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'قريشه', price: '25,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'رقاقات جبنه فاهيتا', price: '40,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'سبانغ كبير', price: '50,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'سبانغ صغير ', price: '20,000 ل.ل', image: 'logozouhbi.PNG' },        
-        { name: 'طرابسيه', price: '25,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'اوزي', price: '100,000 ل.ل' , image: 'logozouhbi.PNG'},
+        { name: 'كروسون جبنه', price: '50,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'كروسون شوكولا', price: '50,000 ل.ل', image: 'logozouhbi.png' },
+        { name: ' بيتزا', price: '40,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'لحمه بعجين صغير', price: '40,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'لحمه بعجين كبير', price: '80,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'قريشه', price: '25,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'رقاقات جبنه فاهيتا', price: '40,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'سبانغ كبير', price: '50,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'سبانغ صغير ', price: '20,000 ل.ل', image: 'logozouhbi.png' },        
+        { name: 'طرابسيه', price: '25,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'اوزي', price: '100,000 ل.ل' , image: 'logozouhbi.png'},
 
 
 
-        { name: 'دزينه رقاقات جبنه', price: '450,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'دزدينه  مشكل', price: '400,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'دزدينه لحمه بعجين صغير', price: '450,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'دزدينه لحمه بعجين كبير', price: '900,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'دزينه رقاقات فاهيتا', price: '450,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'دزينه طرابلسيه', price: '300,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'دزينه بعلبكيه', price: '300,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'دزينه سواريه اريشه ', price: '300,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'دزدينه سواريه سبانغ صغير', price: '240,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'دزدينه سواريه سبانغ كبير', price: '400,000 ل.ل', image: 'logozouhbi.PNG' },
-        { name: 'دزينه سواريه بيتزا', price: '450,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'دزدينه عجينة لحمه بعجين نيه و رقاقات', price: '300,000 ل.ل' , image: 'logozouhbi.PNG'},
+        { name: 'دزينه رقاقات جبنه', price: '450,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'دزدينه  مشكل', price: '400,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'دزدينه لحمه بعجين صغير', price: '450,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'دزدينه لحمه بعجين كبير', price: '900,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'دزينه رقاقات فاهيتا', price: '450,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'دزينه طرابلسيه', price: '300,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'دزينه بعلبكيه', price: '300,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'دزينه سواريه اريشه ', price: '300,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'دزدينه سواريه سبانغ صغير', price: '240,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'دزدينه سواريه سبانغ كبير', price: '400,000 ل.ل', image: 'logozouhbi.png' },
+        { name: 'دزينه سواريه بيتزا', price: '450,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'دزدينه عجينة لحمه بعجين نيه و رقاقات', price: '300,000 ل.ل' , image: 'logozouhbi.png'},
       ]
     },
     kaak: {
       title: '٣. كعك',
       items: [
-        { name: 'كعكه', price: '120,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'كعكه مع درة', price: '150,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'كعكه إكسترا', price: '150,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'كعكه مع خضره', price: '150,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'كعكه فاهيتا', price: '150,000 ل.ل' , image: 'logozouhbi.PNG'},
+        { name: 'كعكه', price: '120,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'كعكه مع درة', price: '150,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'كعكه إكسترا', price: '150,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'كعكه مع خضره', price: '150,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'كعكه فاهيتا', price: '150,000 ل.ل' , image: 'logozouhbi.png'},
 
       ]
     },
     pizza: {
       title: '٤. بيتزا',
       items: [
-        { name: 'ييتزا صغيرة', price: '150,000 ل.ل', image: 'logozouhbi.PNG'},
-        { name: 'ييتزا وسط', price: '300,000 ل.ل' , image: 'logozouhbi.PNG'},
-        { name: 'ييتزا عائلية', price: '900,000 ل.ل' , image: 'logozouhbi.PNG'},
+        { name: 'ييتزا صغيرة', price: '150,000 ل.ل', image: 'logozouhbi.png'},
+        { name: 'ييتزا وسط', price: '300,000 ل.ل' , image: 'logozouhbi.png'},
+        { name: 'ييتزا عائلية', price: '900,000 ل.ل' , image: 'logozouhbi.png'},
         
        { name: 'ييتزا سجق', sizes: { small: '200,000 ل.ل', large: '650,000 ل.ل'  },
-        image: 'logozouhbi.PNG'},
+        image: 'logozouhbi.png'},
 
 
 
@@ -175,7 +175,7 @@ document.addEventListener('DOMContentLoaded', function () {
   if (item.image) {
   const img = document.createElement('img');
   // بدلنا الصورة بالرابط الجديد
-  img.src = 'https://placehold.co/400/logozouhbi.PNG';
+  img.src = 'https://placehold.co/400/logozouhbi.png';
   img.alt = item.name;
   img.classList.add('item-image');
   itemDiv.appendChild(img);
